3b89340d9f8703bed00c6571a803c042075f9a6b
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2024-06-01 01:20:17 +0000

    Database

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+
+
+//const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+//const dotenv = require("dotenv");
+
+const app = express();
+
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    //root route  http://localhost:5000/
+    res.send("Hello World!!");
+});
+
+app.use("/api/auth",authRoutes)
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));

ef245b3b7b4426bc9f616fe0482fb6e817af07ee
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2024-06-03 10:17:27 +0000

    pp

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+    return (
+        <div >
+
+
+            <SearchInput />
+            <div className='divider px-3'></div>
+            <Conversations />
+        </div>
+    )
+}
+
+export default Sidebar;

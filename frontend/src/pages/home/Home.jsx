d41ca221b05efb1bb7652c5c75f64b74fb31c7ef
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2024-06-03 10:17:47 +0000

    dp

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,11 @@
 function Home() {
     return(
-        <div>Home</div>
+        <div className='flex sm-h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding
+        backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <Sidebar />
+            {/* <MessageContainer /> */}
+
+        </div>
     )
 }
 

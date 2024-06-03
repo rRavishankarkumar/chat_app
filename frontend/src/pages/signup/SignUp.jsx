eae7ee0635fb66b0302ef8361fc5404341841e1e
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2024-06-03 03:08:49 +0000

    frnt

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,55 @@
-function signUp() {
-  return(
-      <div>signUp</div>
-  )
-}
+import GenderCheckBox from "./GenderCheckbox";
 
-export default signUp
+const signUp = () => {
+  return (
+        <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+          <h1 className='text-3xl font-semibold text-center text-gary-300'>
+             SignUp<span className='text-blue-500'>ChatApp</span>
+          </h1>
+
+          <form>
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Full Name</span>
+              </label>
+              <input type='text' placeholder='full name' className='w-full input input-border h-10'/>
+            </div>
+
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Username</span>
+              </label>
+              <input type='text' placeholder='full name' className='w-full input input-border h-10'/>
+            </div>
+
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Password</span>
+              </label>
+              <input type='text' placeholder='full name' className='w-full input input-border h-10'/>
+            </div>
+
+            <div>
+              <label className='label p-2'>
+                <span className='text-base label-text'>Confirm Password</span>
+              </label>
+              <input type='text' placeholder='full name' className='w-full input input-border h-10'/>
+            </div>
+
+            <GenderCheckBox />
+
+            <a className='text-sm hover:underline hover:text-blue-600 mt-4 inline-block' href='#'>
+              Already have an account?
+            </a>
+
+            <div>
+              <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+            </div>
+          </form>
+        </div>
+      </div>
+  );
+};
+
+export default signUp;

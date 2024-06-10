470328039adcec792d53dcb65902bddeb6cfd24f
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2024-06-10 20:13:29 +0000

    ,MM

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -13,3 +13,24 @@ const Conversations = () => {
     );
 };
 export default Conversations;
+
+
+
+
+//STARTER CODE FOR SNIPPET
+
+// import Conversation from "./Conversation";
+
+// const Conversations = () => {
+//     return (
+//         <div className='py-2 flex flex-col overflow-auto'>
+//             <Conversation />
+//             <Conversation />
+//             <Conversation />
+//             <Conversation />
+//             <Conversation />
+//             <Conversation />
+//         </div>
+//     );
+// };
+// export default Conversations;
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -14,3 +14,24 @@ const SearchInput = () => {
 }
 
 export default SearchInput;
+
+
+
+
+//Starter code for snippet;
+//import { IoSearchSharp } from "react-icons/io5";
+
+
+
+// const SearchInput = () => {
+//     return (
+//         <form className='flex items-center gap-2'>
+//             <input type="text" placeholder='Search...' className='input input-bordered rounded-full' />
+//             <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+//             <IoSearchSharp className='w-6 h-6 outlinee-none' />
+//             </button>
+//         </form>
+//     )
+// }
+
+// export default SearchInput;

f033753cc93d5b2882f78d639d6496cc6c5a33b8
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2024-06-20 07:37:51 +0000

    rg

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -11,5 +11,5 @@ export const useAuthcontext = () => {
 export const AuthContextProvider = ({ children }) => {
   const [authUser,setAuthUser] = useState(JSON.parse(localStorage.getItem("chat-user")) || null)
 
-  return <AuthContext.Provider value={{authUser, setAuthUser}}>{children}</Authontext.Provider>;
+  return <AuthContext.Provider value={{authUser, setAuthUser}}>{children}</AuthContext.Provider>;
 };

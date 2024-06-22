da4c0e985983ee50f551ce048ac7eb631232a1cf
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2024-06-22 17:09:08 +0000

    backend complited

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const conversationSchema = new mongoose.Schema(
 	{
 		participants: [
 			{
 				type: mongoose.Schema.Types.ObjectId,
-				ref: "User",
+				ref: 'User',
 			},
 		],
 		messages: [
 			{
 				type: mongoose.Schema.Types.ObjectId,
-				ref: "Message",
+				ref: 'Message',
 				default: [],
 			},
 		],
